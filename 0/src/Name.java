66687b47175e210d1a250fec6371013d889ab408
Sir' Bill <Sir' Bill@DESKTOP-95SA5F9>
2018-07-24 11:38:34 +0700

    version1

---
+++ 0/src/Name.java
@@ -0,0 +1,18 @@
+
+public class Name {
+	String name;
+	String lastname;
+	
+	public int getName() {
+		return name;
+	}
+	public void setName() {
+		this.name=name;
+	}
+	public int getLastname() {
+		return lastname;
+	}
+	public void setLastname() {
+		this.lastname=lastname;
+	}
+}
---
+++ 0/src/RunName.java
@@ -0,0 +1,11 @@
+
+public class RunName {
+
+	public static void main(String[] args) {
+		Name n = new Name(bb,bb);
+		n.getName();
+		System.out.print(n.getName);
+
+	}
+
+}
